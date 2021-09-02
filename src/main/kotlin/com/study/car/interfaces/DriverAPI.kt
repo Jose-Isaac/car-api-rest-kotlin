d7299e61c449400c598a2861e5ba9809ad27d20a
Jose Isaac Barreiro Campos <=>
2021-09-02 20:49:32 -0300

    created findById method for driver

--- src/main/kotlin/com/study/car/interfaces/DriverAPI.kt
+++ src/main/kotlin/com/study/car/interfaces/DriverAPI.kt
@@ -2,17 +2,24 @@ package com.study.car.interfaces
 
 import com.study.car.entities.Driver
 import com.study.car.entities.DriverRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
-//@Service
+@Service
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
 class DriverAPI(val driverRepository: DriverRepository) {
 
     @GetMapping("/drivers")
     fun listDrivers() : List<Driver> = driverRepository.findAll()
+
+    @GetMapping("/drivers/{id}")
+    fun findDriver(@PathVariable("id") id : Long) = driverRepository
+        .findById(id).orElseThrow {ResponseStatusException(HttpStatus.NOT_FOUND)}
 }
