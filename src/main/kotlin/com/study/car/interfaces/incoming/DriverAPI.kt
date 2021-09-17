afb7dde7ff3a2997b320207cca53691f0c1bddf0
Jose Isaac Barreiro Campos <=>
2021-09-17 08:34:00 -0300

    ajustments formating in DriverAPI

--- src/main/kotlin/com/study/car/interfaces/incoming/DriverAPI.kt
+++ src/main/kotlin/com/study/car/interfaces/incoming/DriverAPI.kt
@@ -1,12 +1,20 @@
 package com.study.car.interfaces.incoming
 
-import com.study.car.entities.Driver
 import com.study.car.domain.DriverRepository
+import com.study.car.entities.Driver
 import com.study.car.entities.PatchDriver
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 
 @Service
@@ -18,17 +26,17 @@ import org.springframework.web.server.ResponseStatusException
 class DriverAPI(val driverRepository: DriverRepository) {
 
     @GetMapping
-    fun listDrivers() : List<Driver> = driverRepository.findAll()
+    fun listDrivers(): List<Driver> = driverRepository.findAll()
 
     @GetMapping("/{id}")
-    fun findDriver(@PathVariable("id") id : Long) = driverRepository
-        .findById(id).orElseThrow {ResponseStatusException(HttpStatus.NOT_FOUND)}
+    fun findDriver(@PathVariable("id") id: Long): Driver = driverRepository
+        .findById(id).orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND) }
 
     @PostMapping
-    fun createDriver(@RequestBody driver : Driver) = driverRepository.save(driver)
+    fun createDriver(@RequestBody driver: Driver) = driverRepository.save(driver)
 
     @PutMapping("/{id}")
-    fun fullUpdateDriver(@PathVariable("id") id : Long, @RequestBody driver: Driver) : Driver {
+    fun fullUpdateDriver(@PathVariable("id") id: Long, @RequestBody driver: Driver): Driver {
         val foundDriver = findDriver(id)
 
         val copyDriver = foundDriver.copy(
@@ -40,7 +48,7 @@ class DriverAPI(val driverRepository: DriverRepository) {
     }
 
     @PatchMapping("/{id}")
-    fun incrementalUpdateDriver(@PathVariable("id") id : Long, @RequestBody driver : PatchDriver) : Driver {
+    fun incrementalUpdateDriver(@PathVariable("id") id: Long, @RequestBody driver: PatchDriver): Driver {
         val foundDriver = findDriver(id)
 
         val copyDriver = foundDriver.copy(
@@ -52,5 +60,5 @@ class DriverAPI(val driverRepository: DriverRepository) {
     }
 
     @DeleteMapping("/{id}")
-    fun deleteById(@PathVariable("id") id : Long) = driverRepository.delete(findDriver(id))
+    fun deleteById(@PathVariable("id") id: Long) = driverRepository.delete(findDriver(id))
 }
